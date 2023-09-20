0634a2ef5d7531ec31b13804846a2be2c63a7644
wesleycarothers <wesleycarothers@outlook.com>
2023-09-19 20:40:21 -0600

    updated project model. need to update further to allow many images to be associated with the project

--- base/models.py
+++ base/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    #image = models.ImageField(upload_to='portfolio/images/')
+    description = models.TextField()
+    main_image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.title
     
+
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
