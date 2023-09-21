11426060c9da1ad9f26b05eaeaa561a4d06582c8
wesleycarothers <wesleycarothers@outlook.com>
2023-09-21 09:47:14 -0600

    trying to get repo cleaned

--- base/models.py
+++ base/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    #image = models.ImageField(upload_to='portfolio/images/')
+    description = models.TextField()
     
     def __str__(self):
         return self.title
     
+class Image(models.Model):
+    project = models.ForeignKey('Project', related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
