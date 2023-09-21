502ed56be4e15aeada6edc1904bf8a86dd2cf763
wesleycarothers <wesleycarothers@outlook.com>
2023-09-21 11:08:44 -0600

    updated project model to have short description and main image.

--- base/models.py
+++ base/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    short_description = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    main_image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.title
