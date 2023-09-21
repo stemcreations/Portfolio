7decc80be44462374772bc8a58e984feb7a9dcbf
wesleycarothers <wesleycarothers@outlook.com>
2023-09-21 09:33:12 -0600

    updated models to allow image upload.

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Project, Contact
+from .models import Project, Contact, Image
 
 admin.site.register(Project)
 admin.site.register(Contact)
+admin.site.register(Image)
 
--- base/models.py
+++ base/models.py
@@ -3,11 +3,13 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    main_image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.title
     
+class Image(models.Model):
+    project = models.ForeignKey('Project', related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
